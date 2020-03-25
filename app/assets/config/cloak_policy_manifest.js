f86c0bb93cfc84563b34989d8b81d25502021c63
fred schoeneman <fred.schoeneman@gmail.com>
2020-03-24 18:59:39 -0700

    added material assets

--- app/assets/config/cloak_policy_manifest.js
+++ app/assets/config/cloak_policy_manifest.js
@@ -1,3 +1,3 @@
+//= link_tree ../fonts/cloak_policy
+//= link_tree ../images/cloak_policy
 //= link_directory ../stylesheets/cloak_policy .css
-//= link_directory ../images/cloak_policy/extension_icons .png
-//= link cloak_policy/extension_icons/48.png
