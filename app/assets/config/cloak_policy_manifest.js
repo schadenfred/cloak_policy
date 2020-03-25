f646a3909cc5cd2fad249e8734e008566f05c266
fred schoeneman <fred.schoeneman@gmail.com>
2020-03-25 14:54:14 -0700

    try harder

--- app/assets/config/cloak_policy_manifest.js
+++
@@ -1,6 +0,0 @@
-//= link_tree ../fonts
-//= link_tree ../images
-//= link_directory ../stylesheets/cloak_policy .css
-//= link_directory ../javascripts/cloak_policy .js
-
-//= link cloak_policy/application.css
