1a7cfa37aadabc817b70375a34f6f62d46ae1fc4
schadenfred <fred.schoeneman@gmail.com>
2020-06-09 16:25:08 -0700

    change child_type for chocies

--- app/models/concerns/cloak_policy/configurable.rb
+++ app/models/concerns/cloak_policy/configurable.rb
@@ -27,22 +27,22 @@ module CloakPolicy
           unless child.scored_settings.empty?
             intents = []
             child.intents.each do |intent| 
-              intents << { name: intent.name, icon: intent.icon, description: intent.description, weight: intent.weight, child_type: "intent", size: (ring_size / child.scored_settings.count),  selected:  false}
+              intents << { id: intent.id, name: intent.name, icon: intent.icon, description: intent.description, weight: intent.weight, child_type: "intent", size: (ring_size ),  selected:  false}
             end
-            intents << { name: child.name, icon: child.icon, description: child.description, size: ring_size, description: child.description, children: descendants(child.scored_settings) }         
-            array << { name: child.name, icon: child.icon, description: child.description, children: intents}
+            array << { name: child.name, icon: child.icon, description: child.description, size: ring_size, description: child.description, children: descendants(child.scored_settings), intents: intents }         
+            # array << { name: child.name, icon: child.icon, description: child.description, children: intents}
           end
         when child.class.name.eql?('CloakPolicy::Vector')
           intents = []
           child.intents.each do |intent| 
-            intents << { name: intent.name, icon: intent.icon, weight: intent.weight, description: intent.description, child_type: "intent", size: ring_size, selected: false }
+            intents << { id: intent.id, name: intent.name, icon: intent.icon, weight: intent.weight, description: intent.description, child_type: "intent", size: ring_size, selected: false }
           end    
-          intents << { name: child.name, icon: child.icon, description: child.description, children: descendants(child.subvectors) }         
-          array << { name: child.name, icon: child.icon, description: child.description, children: intents }
+          array << { name: child.name, icon: child.icon, description: child.description, children: descendants(child.subvectors), intents: intents }         
+          # array << { name: child.name, icon: child.icon, description: child.description, children: intents }
         when child.class.name.eql?('CloakPolicy::Setting')
           array << { name: child.name, icon: child.icon, description: child.we_say, children: descendants(child.choices) }
         when child.class.name.eql?('CloakPolicy::Choice')
-          array << { name: child.name, icon: child.setting.icon, weight: child.weight, child_type: 'intent',  size: 100, selected: false }
+          array << { name: child.name, icon: child.setting.icon, weight: child.weight, child_type: 'choice',  size: 100, selected: false }
         end
       end
       array
