49742239cefec5d3d355a210ddcda9f56d429b82
schadenfred <fred.schoeneman@gmail.com>
2020-06-17 00:43:43 -0700

    working with both apps

--- app/serializers/cloak_policy/api/v1/choice_serializer.rb
+++ app/serializers/cloak_policy/api/v1/choice_serializer.rb
@@ -1,7 +1,7 @@
 module CloakPolicy
   class Api::V1::ChoiceSerializer
     include FastJsonapi::ObjectSerializer
-    attributes :name, :value, :we_say, :they_say
+    attributes :name, :value, :we_say, :they_say, :weight
 
     belongs_to :setting
   end
